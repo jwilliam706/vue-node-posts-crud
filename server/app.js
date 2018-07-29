d6733955bc9bca2cdb4f002386a04fb959463ba8
William Martinez <williamr1994@gmail.com>
2018-07-28 22:28:01 -0600

    crud completed

---
+++ server/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+const posts = require('./controllers/posts_controller');
+
+const  mongoose = require('mongoose');
+mongoose.connect('mongodb://william:william2018@ds249418.mlab.com:49418/vue-posts',{ useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function(callback){
+  console.log("Connection Succeeded");
+});
+
+app.use('/posts',posts);
+
+app.listen(process.env.PORT || 8081)
---
+++ server/controllers/posts_controller.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const router = express.Router();
+const Post = require("../models/post");
+
+router.get('/', (req, res) => {
+  Post.find({}, 'title description', function (error, posts) {
+    if (error) { console.error(error); }
+    res.send({
+      posts: posts
+    })
+  }).sort({_id:-1})
+})
+
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id, 'title description', function (error, post) {
+    if (error) { console.error(error); }
+    res.send(post)
+  })
+})
+
+router.post('/', (req, res) => {
+  const title = req.body.title;
+  const description = req.body.description;
+  const new_post = new Post({
+    title: title,
+    description: description
+  })
+
+  new_post.save(function (error) {
+    if (error) {
+      console.log(error)
+    }
+    res.send({
+      success: true,
+      message: 'Post saved successfully!'
+    })
+  })
+})
+
+router.put('/:id', (req, res) => {
+  Post.findById(req.params.id, 'title description', function (error, post) {
+    if (error) { console.error(error); }
+
+    post.title = req.body.title
+    post.description = req.body.description
+    post.save(function (error) {
+      if (error) {
+        console.log(error)
+      }
+      res.send({
+        success: true
+      })
+    })
+  })
+})
+
+router.delete('/:id', (req, res) => {
+  Post.remove({
+    _id: req.params.id
+  }, function(err, post){
+    if (err)
+      res.send(err)
+    res.send({
+      success: true
+    })
+  })
+})
+
+module.exports = router;
